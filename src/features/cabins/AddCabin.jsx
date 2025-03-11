23eb96f6cc35a16fdbdd66571cda6eaba845b36a
Orxan Babayev <orxanbabayev.org@gmail.comorxanbabayev.org@gmail.comgit config --global user.nameOrxan-Babayevgit config --global user.email orxanbabayev.org@gmail.com>
2025-03-11 23:50:46 +0400

    Change Modal component to Compound component for better state managment and build reusable Table

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,26 +1,48 @@
 import { useState } from "react";
 import Button from "../../ui/Button";
 import CreateCabinForm from "./CreateCabinForm";
-import Modal from "../../ui/Modal";
+import Modal from "../../ui/Modal.jsx";
 
 function AddCabin() {
-  const [isOpenModal, setIsOpenModal] = useState(false);
   return (
-    <>
-      <Button
-        variation="primary"
-        size="medium"
-        onClick={() => setIsOpenModal((isOpenModal) => !isOpenModal)}
-      >
-        Add new cabine
-      </Button>
-      {isOpenModal && (
-        <Modal onClose={() => setIsOpenModal(false)}>
-          <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
-        </Modal>
-      )}
-    </>
+    <div>
+      <Modal>
+        <Modal.Open opens="cabin-form">
+          <Button>Add new cabin</Button>
+        </Modal.Open>
+        <Modal.Window name="cabin-form">
+          <CreateCabinForm />
+        </Modal.Window>
+
+        {/* <Modal.Open opens="table">
+      <Button>Show table</Button>
+    </Modal.Open>
+    <Modal.Window name="table">
+      <CreateCabinForm />
+    </Modal.Window> */}
+      </Modal>
+    </div>
   );
 }
+
+// function AddCabin() {
+//   const [isOpenModal, setIsOpenModal] = useState(false);
+//   return (
+//     <>
+//       <Button
+//         variation="primary"
+//         size="medium"
+//         onClick={() => setIsOpenModal((isOpenModal) => !isOpenModal)}
+//       >
+//         Add new cabine
+//       </Button>
+//       {isOpenModal && (
+//         <Modal onClose={() => setIsOpenModal(false)}>
+//           <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
+//         </Modal>
+//       )}
+//     </>
+//   );
+// }
 
 export default AddCabin;
--- src/features/settings/useUpdateSetting.js
+++ src/features/settings/useUpdateSetting.js
@@ -2,7 +2,7 @@ import { QueryClient, useMutation } from "@tanstack/react-query";
 import { updateSetting as updatingSettingApi } from "../../services/apiSettings";
 import toast from "react-hot-toast";
 
-export function useEditCabin() {
+export function useUpdateSetting() {
   const { mutate: updateSetting, isLoading: isUpdating } = useMutation({
     mutationFn: ({ settingData }) => updatingSettingApi(settingData),
     onSuccess: () => {
